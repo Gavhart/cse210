eccb2d21dc156c770d8466edb7b17a16bf5a7353
Gavhart <113486288+Gavhart@users.noreply.github.com>
2023-05-24 12:19:27 -0600

    3 done

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -3,11 +3,42 @@ using System;
 class Program
 {
     static void Main(string[] args)
+
+    
     {
-        ScriptureManagement _list = new ScriptureManagement();
-        Reference reference = new Reference(_list.GetSelecterReference());
-        Scripture scripture = new Scripture(reference, _list.GetSelecterScripture());
+        Scripture original = new Scripture();
+        //sets userinput for errors
+        string userInput = "";
 
+        //displays a welcome message
+        DisplayWelcomeMessage();
+
+        //displays original scripture
+        string referance = original.getReferance();
+        string originalScripture = original.getScripture();
+        
+        
+        Console.WriteLine($"{referance}: {originalScripture}");
+
+        //sets a while loop to quit if "quit" is typed
+
+
+        while(userInput != "quit")
+        {
+        
+        userInput = Console.ReadLine();
+        Console.Clear();
+        DisplayScripture scripture = new DisplayScripture(originalScripture);
+        scripture.Display();
+        
+
+        }
+
+
+        static void DisplayWelcomeMessage()
+        {
+            Console.WriteLine("/nWelcome to the Scripture Memorizer, press Enter when you would like words to dissapear. \nType Quit when done.");
+        }
     }
-
 }
+// And by the power of the holy ghost ye may know the truth of all things  Moroni 10:5  5 instances
--- prove/Develop03/scripture.cs
+++ prove/Develop03/scripture.cs
@@ -2,70 +2,33 @@ using System;
 
 class Scripture
 {
-    private string _fileName = "Scriptures.txt";
-    private List<string> _listReferences = new List<string>();
-    private List<string> _listScriptures = new List<string>();
-    private List<string> _tempList = new List<string>();
-    private string line;
-    private string _answer="";
-    private int _answerNumber = 0;
-    private int _counter = 1;
-    private string _tempScripture = "";
-    private string _selectedReference = "";
-    private string _selectedScripture = "";
+    private string referance = "Moroni 10:5";
+    // And by the power of the holy ghost ye may know the truth of all things  Moroni 10:5
+
+    private List<string> scripture = new List<string> {"And ", "by ", "the ", "power ", "of ", "the ", "Holy ", "Ghost ", "ye ", "may ", "know ", "the ", "truth ", "of ", "all ", "things "};
+
+
+
+    public string getScripture()
+    {
+       string variScripture = string.Join("", scripture);
+       return variScripture;
+    }
+
+    public string getReferance()
+    {
+        string refer = referance;
+        return refer;
+    }
+
+    // public void getFirstScripture()
+    // {
+       
     
-    public ScriptureManagement()
-    {
-        LoadFile();
-        ShowReference();
-    }
-    public void LoadFile()
-    {
-        
-        using (StreamReader inputFile = new StreamReader(@_fileName))
-        {
-            
-            while ((line = inputFile.ReadLine()) != null)
-            {
-                string[] _temp = line.Split(';', 2);     
-                foreach (var text in _temp)
-                {_tempList.Add(text);}
-            }
-        }
-        for (int counter=0; counter < _tempList.Count; counter = counter + 2)           
-        {
-            _listReferences.Add(_tempList[counter]);
-            _tempScripture = _tempList[counter + 1];
-            _tempScripture = _tempScripture.Remove(0,1);
-            
-            _listScriptures.Add(_tempScripture);
-
-
-        }
-    }
-    public void ShowReference()
-    {
-        Console.Clear();   
-        Console.WriteLine("Please chose one of the next Scriptures");
-        foreach (var refer in _listReferences)
-        {
-
-            Console.WriteLine(_counter + ".-" + refer);
-            _counter++;
-        }
-        _answer = Console.ReadLine();
-        _answerNumber = int.Parse(_answer);
-        for ( int i = 0; i <_listReferences.Count; i++)
-        {
-            if (_answerNumber ==  i+1)
-            {
-                _selectedReference = _listReferences[i];
-                _selectedScripture = _listScriptures[i];
-            }
-        }
-    }
-
-    public string GetSelecterReference(){ return _selectedReference;}
-    public string GetSelecterScripture(){ return _selectedScripture;}
-
+    //    for(int i=0; i<scripture.Count; i++)
+    //    {
+    //     Console.WriteLine(scripture[i]);
+    //    }
+    
+    // }
 }
